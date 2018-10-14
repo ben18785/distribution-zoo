ff27f8584a2978fda412638a8f759ef80f05e831
Ben Lambert <ben.c.lambert@gmail.com>
2018-10-14 20:07:31 +0100

    Deployed to cloud although am ignoring the file to do this as contains my keys

--- s_Distributions_test.R
+++ s_Distributions_test.R
@@ -2,16 +2,6 @@ rm(list=ls())
 library(shiny)
 setwd("C:/Users/bclamber/Desktop/distribution-viewer")
 runApp("App-1",display.mode = "showcase")
-runExample("02_text") # tables and data frames
-runExample("03_reactivity") # a reactive expression
-runExample("04_mpg") # global variables
-runExample("05_sliders") # slider bars
-runExample("06_tabsets") # tabbed panels
-runExample("07_widgets") # help text and submit buttons
-runExample("08_html") # Shiny app built from HTML
-runExample("09_upload") # file upload wizard
-runExample("10_download") # file download wizard
-runExample("11_timer") # an automated timer
 
 
 n <- 10000
