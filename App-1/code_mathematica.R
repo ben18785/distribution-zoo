1c0a714983bdac49c638dc76caf01f6fbed02c8a
Ben Lambert <ben.c.lambert@gmail.com>
2018-12-03 00:51:28 +0000

    Mathematica seems to work ok now

--- App-1/code_mathematica.R
+++ App-1/code_mathematica.R
@@ -1,25 +1,26 @@
-fPrismMathematica <- function(input){
-  return(prismCodeBlock(text, language = "matlab"))
-}
 
 
 fMathematicacode <- function(input){
   if(input$dist=="Normal"){
     if(input$property=="pdf")
-      fMakeFunctionPaste(mainName="PDF[NormalDistribution",
+      text <- 
+        fMakeFunctionPaste(mainName="PDF[NormalDistribution",
                     params=c(input$mu,input$sigma),
                     postfixparams="x",
                     mathematica=TRUE)
     else if(input$property=="log_pdf")
-      fMakeFunctionPaste(mainName=paste0("-0.5 Log[2 Pi] - Log[", eval(parse(text=input$sigma)),
+      text <-
+        fMakeFunctionPaste(mainName=paste0("-0.5 Log[2 Pi] - Log[", eval(parse(text=input$sigma)),
                                     "] - (x - ",
                                     eval(parse(text=input$mu)), ")^2 / (2 ",
                                     eval(parse(text=input$sigma)), "^2)"),
                     freeform=TRUE)
     else if(input$property=="random")
-      fMakeFunctionPaste(mainName="RandomVariate[NormalDistribution",
+      text <- 
+        fMakeFunctionPaste(mainName="RandomVariate[NormalDistribution",
                     params=c(input$mu,input$sigma),
                     postfixparams="n",
                     mathematica=TRUE)
   }
+  return(prismCodeBlock(text))
 }
