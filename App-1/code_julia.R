79e308722716aa96db893a110030e7f5c41e7b48
Ben Lambert <ben.c.lambert@gmail.com>
2018-12-03 00:49:30 +0000

    Need to look at Julia code

--- App-1/code_julia.R
+++ App-1/code_julia.R
@@ -1,23 +1,23 @@
 fJuliacode <- function(input){
   if(input$dist=="Normal"){
     if(input$property=="pdf")
-      short <- fMakeFunction(mainName="pdf",
+      text <- fMakeFunctionPaste(mainName="pdf",
                              params=c(input$mu,input$sigma),
                              postfixparams="x",
                              julia=TRUE)
     else if(input$property=="log_pdf")
-      short <- fMakeFunction(mainName="logpdf",
+      text <- fMakeFunctionPaste(mainName="logpdf",
                              params=c(input$mu,input$sigma),
                              postfixparams="x",
                              julia=TRUE)
     else if(input$property=="random")
-      short <- fMakeFunction(mainName="rand(Normal",
+      text <- fMakeFunctionPaste(mainName="rand(Normal",
                              params=c(input$mu,input$sigma),
                              postfixparams="n",
                              julia=TRUE)
   }
   
-  tagList(short,
+  tagList(prismCodeBlock(text, language = "julia"),
           h3("Note that code assumes that 'Compat' and 'Distributions' packages are installed by typing:"),
           h3("Pkg.add(\"Compat\")", style="color:#d95f02"),
           h3("Pkg.add(\"Distributions\")", style="color:#d95f02"),
