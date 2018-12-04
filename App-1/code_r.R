38f21cc16b2616fae160064a08169923a908df3c
Ben Lambert <ben.c.lambert@gmail.com>
2018-12-04 01:39:10 +0000

    Simplified construction of R functions

--- App-1/code_r.R
+++ App-1/code_r.R
@@ -1,20 +1,25 @@
 
+fRHelper <- function(mainName, params, input){
+  if(input$property=="pdf")
+    fMakeFunctionPaste(mainName=paste0("d", mainName),
+                     params=params,
+                     prefixparams="x")
+  else if(input$property=="log_pdf")
+    fMakeFunctionPaste(mainName=paste0("d", mainName),
+                       params=params,
+                       prefixparams="x",
+                       postfixparams="log=TRUE")
+  else if(input$property=="random")
+    fMakeFunctionPaste(mainName=paste0("r", mainName),
+                       params=params,
+                       prefixparams="n")
+}
+
+
 fRcode <- function(input){
   text <-
     if(input$dist=="Normal"){
-      if(input$property=="pdf")
-        fMakeFunctionPaste(mainName="dnorm",
-                                          params=c(input$mu,input$sigma),
-                                          prefixparams="x")
-      else if(input$property=="log_pdf")
-        fMakeFunctionPaste(mainName="dnorm",
-                      params=c(input$mu,input$sigma),
-                      prefixparams="x",
-                      postfixparams="log=TRUE")
-      else if(input$property=="random")
-        fMakeFunctionPaste(mainName="rnorm",
-                      params=c(input$mu,input$sigma),
-                      prefixparams="n")
+      fRHelper("norm", c(input$mu, input$sigma), input)
     }else if(input$dist=="Uniform"){
         if(input$property=="pdf")
           fMakeFunctionPaste(mainName="dunif",
@@ -28,6 +33,20 @@ fRcode <- function(input){
       else if(input$property=="random")
         fMakeFunctionPaste(mainName="runif",
                            params=c(input$a,input$b),
+                           prefixparams="n")
+    }else if(input$dist=="LogNormal"){
+      if(input$property=="pdf")
+        fMakeFunctionPaste(mainName="dlnnorm",
+                           params=c(input$meanlog,input$sdlog),
+                           prefixparams="x")
+      else if(input$property=="log_pdf")
+        fMakeFunctionPaste(mainName="dlnorm",
+                           params=c(input$meanlog,input$sdlog),
+                           prefixparams="x",
+                           postfixparams="log=TRUE")
+      else if(input$property=="random")
+        fMakeFunctionPaste(mainName="rlnorm",
+                           params=c(input$meanlog,input$sdlog),
                            prefixparams="n")
     }
   return(prismCodeBlock(text, language = "r"))
