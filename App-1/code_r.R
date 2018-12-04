c383ad3d4f8d4f391f43401bb06c50961bd9d05f
Ben Lambert <ben.c.lambert@gmail.com>
2018-12-04 01:56:18 +0000

    Up to Cauchy now done for R

--- App-1/code_r.R
+++ App-1/code_r.R
@@ -1,53 +1,28 @@
 
 fRHelper <- function(mainName, params, input){
-  if(input$property=="pdf")
-    fMakeFunctionPaste(mainName=paste0("d", mainName),
-                     params=params,
-                     prefixparams="x")
-  else if(input$property=="log_pdf")
-    fMakeFunctionPaste(mainName=paste0("d", mainName),
-                       params=params,
-                       prefixparams="x",
-                       postfixparams="log=TRUE")
-  else if(input$property=="random")
-    fMakeFunctionPaste(mainName=paste0("r", mainName),
-                       params=params,
-                       prefixparams="n")
+  switch(input$property,
+        pdf=fMakeFunctionPaste(mainName=paste0("d", mainName),
+                               params=params, prefixparams="x"),
+        log_pdf=fMakeFunctionPaste(mainName=paste0("d", mainName),
+                                   params=params, prefixparams="x",
+                                   postfixparams="log=TRUE"),
+        random=fMakeFunctionPaste(mainName=paste0("r", mainName),
+                                  params=params, prefixparams="n"))
 }
 
 
 fRcode <- function(input){
   text <-
-    if(input$dist=="Normal"){
-      fRHelper("norm", c(input$mu, input$sigma), input)
-    }else if(input$dist=="Uniform"){
-        if(input$property=="pdf")
-          fMakeFunctionPaste(mainName="dunif",
-                             params=c(input$a,input$b),
-                             prefixparams="x")
-        else if(input$property=="log_pdf")
-          fMakeFunctionPaste(mainName="dunif",
-                           params=c(input$a,input$b),
-                           prefixparams="x",
-                           postfixparams="log=TRUE")
-      else if(input$property=="random")
-        fMakeFunctionPaste(mainName="runif",
-                           params=c(input$a,input$b),
-                           prefixparams="n")
-    }else if(input$dist=="LogNormal"){
-      if(input$property=="pdf")
-        fMakeFunctionPaste(mainName="dlnnorm",
-                           params=c(input$meanlog,input$sdlog),
-                           prefixparams="x")
-      else if(input$property=="log_pdf")
-        fMakeFunctionPaste(mainName="dlnorm",
-                           params=c(input$meanlog,input$sdlog),
-                           prefixparams="x",
-                           postfixparams="log=TRUE")
-      else if(input$property=="random")
-        fMakeFunctionPaste(mainName="rlnorm",
-                           params=c(input$meanlog,input$sdlog),
-                           prefixparams="n")
-    }
+    switch(input$dist,
+           Normal=fRHelper("norm", c(input$mu, input$sigma), input),
+           Uniform=fRHelper("unif", c(input$a, input$b), input),
+           LogNormal=fRHelper("lnorm", c(input$meanlog,input$sdlog), input),
+           Exponential=fRHelper("exp", input$rate, input),
+           Gamma=fRHelper("gamma", c(input$shape, input$rateGam), input),
+           t=fRHelper("st", c(input$muT,input$sigmaT, input$nuT), input),
+           Beta=fRHelper("beta", c(input$alpha,input$beta), input),
+           Cauchy=fRHelper("cauchy", c(input$locationC,input$scaleC), input))
+           
+  
   return(prismCodeBlock(text, language = "r"))
 }
