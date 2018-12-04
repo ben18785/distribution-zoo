6a904d29ade33991e6dc3b824461664e7cdca445
Ben Lambert <ben.c.lambert@gmail.com>
2018-12-04 02:15:55 +0000

    Custom half cauchy

--- App-1/code_r.R
+++ App-1/code_r.R
@@ -14,6 +14,33 @@ fRHelper <- function(mainName, params, input, import=NULL){
 }
 
 
+dHalfCauchy <- paste("dHalfCauchy <- function(x, location, scale, log=FALSE){",
+                     "if(log=FALSE){",
+                     "  if(x >= 0)",
+                     "    return(1.0 / (pi * (1 + ((x-location)/scale)^2) * scale * (0.5 + atan(location / scale) / pi)))",
+                     "  else",
+                     "    return(0.0)",
+                     "}else{",
+                     "  if(x >= 0)",
+                     "    return(log(1.0 / (pi * (1 + ((x-location)/scale)^2) * scale * (0.5 + atan(location / scale) / pi))))",
+                     "  else",
+                     "    return(-Inf)",
+                     sep = "\n")
+
+dHalfCauchyLog <- paste("dHalfCauchy <- function(x, location, scale, log=TRUE){",
+                     "if(log=FALSE){",
+                     "  if(x >= 0)",
+                     "    return(1.0 / (pi * (1 + ((x-location)/scale)^2) * scale * (0.5 + atan(location / scale) / pi)))",
+                     "  else",
+                     "    return(0.0)",
+                     "}else{",
+                     "  if(x >= 0)",
+                     "    return(log(1.0 / (pi * (1 + ((x-location)/scale)^2) * scale * (0.5 + atan(location / scale) / pi))))",
+                     "  else",
+                     "    return(-Inf)",
+                     sep = "\n")
+
+
 fRcode <- function(input){
   text <-
     switch(input$dist,
@@ -24,7 +51,8 @@ fRcode <- function(input){
            Gamma=fRHelper("gamma", c(input$shape, input$rateGam), input),
            t=fRHelper("st", c(input$muT,input$sigmaT, input$nuT), input, import="library(LaplacesDemon)"),
            Beta=fRHelper("beta", c(input$alpha,input$beta), input),
-           Cauchy=fRHelper("cauchy", c(input$locationC,input$scaleC), input))
+           Cauchy=fRHelper("cauchy", c(input$locationC,input$scaleC), input),
+           HalfCauchy=)
            
   
   return(prismCodeBlock(text, language = "r"))
