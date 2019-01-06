4b6538b25ab9da356fb272dd26a72421218c1307
Ben Lambert <ben.c.lambert@gmail.com>
2019-01-06 23:57:17 +0000

    Latex/normal

--- App-1/code_latex.R
+++ App-1/code_latex.R
@@ -18,14 +18,11 @@ fLatexHelper <- function(lMoments, lPDF, lCDF){
 fLatex <- function(input){
   if (input$distType=='Continuous'){
     switch(input$dist,
-           Normal=tagList(h2("Moments"),
-              fPrismLatex("\\mathrm{E}(X) = \\mu"),
-              fPrismLatex("var(X) = \\sigma^2"),
-              h2("PDF"),
-              fPrismLatex("f(x|\\mu,\\sigma) = \\frac{1}{\\sqrt{2\\pi\\sigma^2}}
-                          \\text{exp}\\left(-\\frac{(x-\\mu)^2}{2\\sigma^2}\\right)"),
-              h2("CDF"),
-              fPrismLatex("F(x|\\mu,\\sigma) = \\frac{1}{2}\\left[1+\\text{erf}
+           Normal=fLatexHelper(c("\\mathrm{E}(X) = \\mu",
+                                 "var(X) = \\sigma^2"),
+                               c("f(x|\\mu,\\sigma) = \\frac{1}{\\sqrt{2\\pi\\sigma^2}}
+                                 \\text{exp}\\left(-\\frac{(x-\\mu)^2}{2\\sigma^2}\\right)"),
+                               c("F(x|\\mu,\\sigma) = \\frac{1}{2}\\left[1+\\text{erf}
                           \\left(\\frac{x-\\mu}{\\sigma\\sqrt{2}}\\right)\\right]")),
            Uniform=fLatexHelper(c("\\mathrm{E}(X) = \\frac{1}{2}(a + b)",
                                   "var(X) = \\frac{1}{12}(b - a)^2"), 
