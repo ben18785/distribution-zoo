9ad317e8b29be382c8b7a2e662e0ae8ef9be2141
Ben Lambert <ben.c.lambert@gmail.com>
2019-01-06 00:01:50 +0000

    Many Stan distributions

--- App-1/code_stan.R
+++ App-1/code_stan.R
@@ -16,31 +16,47 @@ fStanHelper <- function(mainName, params, input, import=NULL,
                                   vector_params=vector_params))
 }
 
+fStanHelper_discrete <- function(mainName, params, input, import=NULL,
+                        named_arguments=NULL, vector_params=FALSE, end_brace=FALSE){
+  switch(input$property,
+         pdf=fMakeFunctionPaste_stan(mainName=paste0("exp(", mainName, "_lpmf"),
+                                     params=params, prefixparams="x",
+                                     import=import, named_arguments=named_arguments,
+                                     vector_params=vector_params, end_brace = T),
+         log_pdf=fMakeFunctionPaste_stan(mainName=paste0(mainName, "_lpmf"),
+                                         params=params, prefixparams="x",
+                                         import=import, named_arguments=named_arguments,
+                                         vector_params=vector_params),
+         random=fMakeFunctionPaste_stan(mainName=paste0(mainName, "_rng"),
+                                        params=params,
+                                        import=import, named_arguments=named_arguments,
+                                        vector_params=vector_params))
+}
+
 
 fStanCode <- function(input){
   text <-
     if(input$distType=='Continuous'){
       switch(input$dist,
              Normal=fStanHelper("normal", c(input$normal_mu, input$normal_sigma), input),
-             Uniform=fStanHelper("unif", c(input$uniform_a, input$uniform_b), input),
-             LogNormal=fStanHelper("lnorm", c(input$lognormal_mu,input$lognormal_sigma), input),
-             Exponential=fStanHelper("exp", input$exponential_rate, input),
+             Uniform=fStanHelper("uniform", c(input$uniform_a, input$uniform_b), input),
+             LogNormal=fStanHelper("lognormal", c(input$lognormal_mu,input$lognormal_sigma), input),
+             Exponential=fStanHelper("exponential", input$exponential_rate, input),
              Gamma=fStanHelper("gamma", c(input$gamma_shape, input$gamma_rate), input),
-             t=fStanHelper("st", c(input$t_mu,input$t_sigma, input$t_nu), input, import="library(LaplacesDemon)"),
+             t=fStanHelper("student_t", c(input$t_nu, input$t_mu,input$t_sigma), input),
              Beta=fStanHelper("beta", c(input$beta_a,input$beta_b), input),
              Cauchy=fStanHelper("cauchy", c(input$cauchy_location,input$cauchy_scale), input),
-             HalfCauchy=dHalfCauchyFull(input$halfcauchy_location, input$halfcauchy_scale, input),
-             InverseGamma=fStanHelper("invgamma", c(input$inversegamma_shape, 1.0 / input$inversegamma_scale), input, import="library(actuar)"),
-             InverseChiSquared=fStanHelper("invchisq", input$inversechisquared_df, input, import="library(LaplacesDemon)"),
-             LogitNormal=fStanHelper("logitnorm", c(input$logitnormal_mu, input$logitnormal_sigma), input, import="library(logitnorm)"))
+             HalfCauchy=fStanHelper(input$halfcauchy_location, input$halfcauchy_scale, input),
+             InverseGamma=fStanHelper("inv_gamma", c(input$inversegamma_shape, 1.0 / input$inversegamma_scale), input),
+             InverseChiSquared=fStanHelper("inv_chi_square", input$inversechisquared_df, input),
+             LogitNormal=fStanHelper("logitnorm", c(input$logitnormal_mu, input$logitnormal_sigma), input))
     }else if(input$distType=='Discrete'){
       switch(input$dist1,
-             Bernoulli=fStanHelper("binom", c(1, input$bernoulli_prob), input),
-             Binomial=fStanHelper("binom", c(input$binomial_size, input$binomial_prob), input),
-             Poisson=fStanHelper("pois", input$poisson_lambda, input),
-             NegativeBinomial=fStanHelper("nbinom", c(input$negativebinomial_mean, input$negativebinomial_dispersion), input,
-                                       named_arguments=c("mu", "size")),
-             BetaBinomial=dBetaBinomialFull(input$betabinomial_size, input$betabinomial_shape1, input$betabinomial_shape2, input)
+             Bernoulli=fStanHelper_discrete("bernoulli", input$bernoulli_prob, input),
+             Binomial=fStanHelper_discrete("binomial", c(input$binomial_size, input$binomial_prob), input),
+             Poisson=fStanHelper_discrete("poisson", input$poisson_lambda, input),
+             NegativeBinomial=fStanHelper_discrete("neg_binomial_2", c(input$negativebinomial_mean, input$negativebinomial_dispersion), input),
+             BetaBinomial=fStanHelper_discrete("beta_binomial", c(input$betabinomial_size, input$betabinomial_shape1, input$betabinomial_shape2), input)
              )
     }else if(input$distType=='Multivariate'){
       switch(input$dist2,
