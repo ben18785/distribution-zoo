f5f4ad24a86e7efc3b85c312032f92a364168064
Fergus Cooper <fcooper8472@gmail.com>
2019-04-03 20:50:53 +0100

    #50 C++ for Normal, and "coming soon" for beta

--- App-1/code_cplusplus.R
+++ App-1/code_cplusplus.R
@@ -1,7 +1,7 @@
 fCpluspluscode <- function(input){
   text <- 
   if(input$dist=="Normal"){
-    common.pdf <- paste0(
+    text.common <- paste0(
       "#include &lt;cassert&gt;\n",
       "#include &lt;cmath&gt;\n",
       "\n",
@@ -21,75 +21,108 @@ fCpluspluscode <- function(input){
       "            : mMean(mean), mStdDev(std_dev) {\n",
       "\n",
       "        // Standard deviation must be positive\n",
-      "        assert(mStdDev > 0.0);\n",
+      "        assert(mStdDev &gt; 0.0);\n",
       "\n",
+      "        m2SigSq = 2.0 * mStdDev * mStdDev;\n",
+      "        mPrefactor = 1.0 / std::sqrt(M_PI * m2SigSq);\n",
+      "        mLogPrefactor = -0.5 * std::log(M_PI * m2SigSq);\n",
+      "    }\n",
       "\n",
+      "    double Pdf(const double x) {\n",
+      "        return mPrefactor * exp(-(x - mMean) * (x - mMean) / m2SigSq);\n",
+      "    }\n",
       "\n",
-      "\n",
-      "\n",
-      "\n",
-      "\n",
-      "\n",
-      "\n",
-      "\n",
-      "\n",
-      "\n",
-      "\n"
+      "    double LogPdf(const double x) {\n",
+      "        return mLogPrefactor - (x - mMean) * (x - mMean) / m2SigSq;\n",
+      "    }\n",
+      "};\n",
+      ""
     )
+
+    text.pdf <- paste0(
+      "int main() {\n",
+      "\n",
+      "    // Create distribution with required parameters\n",
+      "    NormalDistributionPdf pdf{", eval(parse(text=input$normal_mu)), ", ", eval(parse(text=input$normal_sigma)), "};\n",
+      "\n",
+      "    // Sample from the pdf at any given x-value:\n",
+      "    pdf.Pdf(0.5);\n",
+      "\n",
+      "    return 0;\n",
+      "}\n",
+      ""
+    )
+
+    text.logpdf <- paste0(
+      "int main() {\n",
+      "\n",
+      "    // Create distribution with required parameters\n",
+      "    NormalDistributionPdf pdf{", eval(parse(text=input$normal_mu)), ", ", eval(parse(text=input$normal_sigma)), "};\n",
+      "\n",
+      "    // Sample from the log pdf at any given x-value:\n",
+      "    pdf.LogPdf(0.5);\n",
+      "\n",
+      "    return 0;\n",
+      "}\n",
+      ""
+    )
+
+    text.samples <- paste0(
+      "#include &lt;random&gt;\n",
+      "\n",
+      "int main() {\n",
+      "    // Seed a Mersenne twister with a 'true' random seed from random_device\n",
+      "    std::random_device rd{};\n",
+      "    std::mt19937 gen{rd()};\n",
+      "\n",
+      "    // Params\n",
+      "    const std::size_t n = 100'000;\n",
+      "    const double mean = ", eval(parse(text=input$normal_mu)), ";\n",
+      "    const double std_dev = ", eval(parse(text=input$normal_sigma)), ";\n",
+      "\n",
+      "    // Create distribution\n",
+      "    std::normal_distribution&lt;double&gt; dist{mean, std_dev};\n",
+      "\n",
+      "    // Create and fill the vector\n",
+      "    std::vector&lt;double&gt; vec(n);\n",
+      "    for (double &x : vec) {\n",
+      "        x = dist(gen);\n",
+      "    }\n",
+      "\n",
+      "    return 0;\n",
+      "}\n",
+      ""
+    )
+
     if(input$property=="pdf")
-      paste(common.pdf, "#include &lt;math&gt",
-              paste("double normal_pdf(int n, double mu, double sigma)",
-                           "{",
-                           paste0("return 1.0 / (std::sqrt(2.0 * M_PI) * ",
-                                  eval(parse(text=input$normal_sigma)),
-                                  ") * exp(-pow(x - ",
-                                  eval(parse(text=input$normal_mu)),
-                                  ", 2.0) / (2 * pow(",
-                                  input$normal_sigma, ", 2.0)));"),
-                           "}",
-                           sep="\n"),
-              sep="\n")
+      paste(text.common, text.pdf, sep="\n")
     else if(input$property=="log_pdf")
-      paste("#include &lt;math&gt",
-                paste("double normal_lpdf(int n, double mu, double sigma)",
-                           "{",
-                           paste0("return -0.5 * log(2 * M_PI) - log(",
-                                  eval(parse(text=input$normal_sigma)),
-                                  ") - pow(x - ",
-                                  eval(parse(text=input$normal_mu)),
-                                  ", 2.0) / (2 * pow(",
-                                  input$normal_sigma, ", 2.0)));"),
-                           "}",
-                           sep="\n"), sep="\n")
+      paste(text.common, text.logpdf, sep="\n")
     else if(input$property=="random")
-      paste("#include &lt;random&gt;",
-                           "#include &lt;vector&gt",
-                           "#include &lt;math&gt",
-                           "#include &lt;chrono&gt;",
-              "// unseeded",
-              paste0("std::vector&lt;double&gt; normal_rng(int n, double mu, double sigma)", "\n",
-                            "{",
-                            "\n",
-                            "unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();", "\n",
-                            "std::normal_distribution&lt;double&gt; distribution(",
-                            eval(parse(text=input$normal_mu)), ", ", eval(parse(text=input$normal_sigma)), ");", "\n",
-                            "std::vector&lt;double&gt; samples(n);", "\n",
-                            "for(int i = 0; i < n; i++)", "\n",
-                            "&emsp;", "samples[i] = distribution(generator);", "\n",
-                            "return samples;", "\n",
-                            "}"),
-              "// seeded",
-              paste0("std::vector&lt;double&gt; normal_rng(int n, double mu, double sigma, unsigned seed)", "\n",
-                            "{",
-                            "\n",
-                            "std::normal_distribution&lt;double&gt; distribution(",
-                            eval(parse(text=input$normal_mu)), ", ", eval(parse(text=input$normal_sigma)), ");", "\n",
-                            "std::vector&lt;double&gt; samples(n);", "\n",
-                            "for(int i = 0; i < n; i++)", "\n",
-                            "&emsp;", "samples[i] = distribution(generator);", "\n",
-                            "return samples;", "\n",
-                            "}"),
-              sep = "\n")
+      paste(text.samples)
+  } else if(input$dist=="Beta"){
+    text.common <- paste0(
+      "Coming soon!"
+    )
+
+    text.pdf <- paste0(
+      ""
+    )
+
+    text.logpdf <- paste0(
+      ""
+    )
+
+    text.samples <- paste0(
+      "Coming soon!"
+    )
+
+    if(input$property=="pdf")
+      paste(text.common, text.pdf, sep="\n")
+    else if(input$property=="log_pdf")
+      paste(text.common, text.logpdf, sep="\n")
+    else if(input$property=="random")
+      paste(text.samples)
   }
   return(prismCodeBlock(text, language = "cpp"))
 }
