3dc46ff1f5456c0fd0157bffccd1eb54cc783183
ben18785 <ben.c.lambert@gmail.com>
2019-10-29 20:46:14 +0000

    added Julia / Uniform, LogNormal, Exponential, Gamma

--- App-1/code_julia.R
+++ App-1/code_julia.R
@@ -1,29 +1,32 @@
-fJuliaHelperTwoParam <- function(name, input, param1, param2){
+fJuliaHelper <- function(name, input, params){
     if(input$property=="pdf")
       fMakeFunctionPaste(mainName=paste0("pdf(", input$dist),
-                         params=c(param1, param2),
+                         params=paste(params, sep = ", "),
                          postfixparams="x",
                          julia=TRUE)
     else if(input$property=="log_pdf")
       fMakeFunctionPaste(mainName=paste0("logpdf(", input$dist),
-                         params=c(param1, param2),
+                         params=paste(params, sep = ", "),
                          postfixparams="x",
                          julia=TRUE)
     else if(input$property=="random")
       fMakeFunctionPaste(mainName=paste0("rand(", input$dist),
-                         params=c(param1, param2),
+                         params=paste(params, sep = ", "),
                          postfixparams="n",
                          julia=TRUE)
 }
 
 
 fJuliacode <- function(input){
-  text <- 
-    if(input$dist=="Normal"){
-      fJuliaHelperTwoParam("Normal", input, input$normal_mu, input$normal_sigma)
-    }else{
-      "To be completed."
-    }
+  text <- switch (input$dist,
+    Normal=fJuliaHelper("Normal", input, c(input$normal_mu, input$normal_sigma)),
+    Uniform=fJuliaHelper("Uniform", input, c(input$uniform_a, input$uniform_b)),
+    LogNormal=fJuliaHelper("LogNormal", input, c(input$lognormal_mu, input$lognormal_sigma)),
+    Exponential=fJuliaHelper("Exponential", input, c(1 / input$exponential_rate)),
+    Gamma=fJuliaHelper("Gamma", input, c(input$gamma_shape, 1 / input$gamma_rate)),
+    "To be completed."
+  )
+      
   
   if(text!="To be completed."){
   tagList(prismCodeBlock("using Random, Distributions", language = "julia"),
