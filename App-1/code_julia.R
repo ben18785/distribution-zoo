81bba1cd76d17b58fa0186be5513e005ede69f28
ben18785 <ben.c.lambert@gmail.com>
2019-10-30 13:36:39 +0000

    julia/bernoulli and julia/binomial

--- App-1/code_julia.R
+++ App-1/code_julia.R
@@ -16,6 +16,24 @@ fJuliaHelper <- function(name, input, params){
                          julia=TRUE)
 }
 
+fJuliaHelperDiscrete <- function(name, input, params){
+  if(input$property=="pdf")
+    fMakeFunctionPaste(mainName=paste0("pdf(", input$dist1),
+                       params=paste(params, sep = ", "),
+                       postfixparams="x",
+                       julia=TRUE)
+  else if(input$property=="log_pdf")
+    fMakeFunctionPaste(mainName=paste0("logpdf(", input$dist1),
+                       params=paste(params, sep = ", "),
+                       postfixparams="x",
+                       julia=TRUE)
+  else if(input$property=="random")
+    fMakeFunctionPaste(mainName=paste0("rand(", input$dist1),
+                       params=paste(params, sep = ", "),
+                       postfixparams="n",
+                       julia=TRUE)
+}
+
 fTCustomJulia <- function(input){
   if(input$property=="pdf")
     paste0("pdf(TDist(", input$t_nu, "), ", "(x - ", input$t_mu, ") / ", input$t_sigma, ") / ", input$t_sigma)
@@ -42,22 +60,28 @@ fInverseChiSquaredJulia <- function(input){
 
 
 fJuliacode <- function(input){
-  text <- switch (input$dist,
-    Normal=fJuliaHelper("Normal", input, c(input$normal_mu, input$normal_sigma)),
-    Uniform=fJuliaHelper("Uniform", input, c(input$uniform_a, input$uniform_b)),
-    LogNormal=fJuliaHelper("LogNormal", input, c(input$lognormal_mu, input$lognormal_sigma)),
-    Exponential=fJuliaHelper("Exponential", input, c(1 / input$exponential_rate)),
-    Gamma=fJuliaHelper("Gamma", input, c(input$gamma_shape, 1 / input$gamma_rate)),
-    t=fTCustomJulia(input),
-    Beta=fJuliaHelper("Beta", input, c(input$beta_a, input$beta_b)),
-    Cauchy=fJuliaHelper("Cauchy", input, c(input$cauchy_location,input$cauchy_scale)),
-    HalfCauchy=fTruncatedCauchyJulia(input),
-    InverseGamma=fJuliaHelper("InverseGamma", input, c(input$inversegamma_shape, input$inversegamma_scale)),
-    InverseChiSquared=fInverseChiSquaredJulia(input),
-    LogitNormal=fJuliaHelper("LogitNormal", input, c(input$logitnormal_mu, input$logitnormal_sigma)),
-    "Coming soon."
-  )
-      
+  text <- 
+    if(input$distType=='Continuous'){
+      switch(input$dist,
+              Normal=fJuliaHelper("Normal", input, c(input$normal_mu, input$normal_sigma)),
+              Uniform=fJuliaHelper("Uniform", input, c(input$uniform_a, input$uniform_b)),
+              LogNormal=fJuliaHelper("LogNormal", input, c(input$lognormal_mu, input$lognormal_sigma)),
+              Exponential=fJuliaHelper("Exponential", input, c(1 / input$exponential_rate)),
+              Gamma=fJuliaHelper("Gamma", input, c(input$gamma_shape, 1 / input$gamma_rate)),
+              t=fTCustomJulia(input),
+              Beta=fJuliaHelper("Beta", input, c(input$beta_a, input$beta_b)),
+              Cauchy=fJuliaHelper("Cauchy", input, c(input$cauchy_location,input$cauchy_scale)),
+              HalfCauchy=fTruncatedCauchyJulia(input),
+              InverseGamma=fJuliaHelper("InverseGamma", input, c(input$inversegamma_shape, input$inversegamma_scale)),
+              InverseChiSquared=fInverseChiSquaredJulia(input),
+              LogitNormal=fJuliaHelper("LogitNormal", input, c(input$logitnormal_mu, input$logitnormal_sigma)),
+              "Coming soon.")
+    }else if(input$distType=='Discrete'){
+       switch(input$dist1,
+              Bernoulli=fJuliaHelperDiscrete("Bernoulli", input, c(input$bernoulli_prob)),
+              Binomial=fJuliaHelperDiscrete("Binomial", input, c(input$binomial_size, input$binomial_prob)),
+              "Coming soon.")
+    }
   
   if(text!="Coming soon."){
   tagList(prismCodeBlock(paste0("using Random, Distributions\n",
