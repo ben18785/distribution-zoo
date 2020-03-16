db5f2b8978049f4beec7d8de09d631921a4d8a9b
Fergus Cooper <fcooper8472@gmail.com>
2020-03-16 16:04:36 +0000

    Add a little more information in analytics

--- App-1/ui.R
+++ App-1/ui.R
@@ -31,7 +31,7 @@ shinyUI(fluidPage(
   headerPanel("The distribution zoo"),
   tagList(h4("by")),
   fluidRow(h4(ben_link, " and ", fergus_link)),
-  fluidRow(h4("Used by ", ga_data['total'], " people in ", length(ga_data) - 1, " countries in the last month")),
+  fluidRow(h4("Used by ", ga_data['user_count'], " people ", ga_data['session_count'], "times in ", ga_data['country_count'], " countries in the last month")),
   prismDependencies,
   prismLanguageDependencies(c("r", "python", "latex",
                               "matlab", "mathematica", "c-like",
